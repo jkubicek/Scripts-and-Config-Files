c6b3a1a267e3befa3648aa8aeb6949cd898da95e
Jim Kubicek <jkubicek@gmail.com>
2012-05-22 06:48:08 -0700

    created input rc file

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 ln -Ff bash_profile ~/.bash_profile
 
 # Install Git Completion
-ln -Ff /usr/local/git/contrib/completion/git-completion.bash ~/.git-completion.sh
+ln -Ff /usr/local/git/contrib/completion/git-completion.bash ~/.git-completion.sh 
 
 # Set some Git config options
 git config --global color.ui true
+
+# Setup smarter autocompletion in Bash
+ln -Ff inputrc ~/.inputrc
